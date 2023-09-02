f7e29bb359099456600ebbf45408ef01e8c80067
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-02 09:41:34 +0200

    use table component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from './components/table/table.component';
 
 @Component({
 	selector: 'challenger-root',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, TableComponent],
 	templateUrl: './app.component.html',
 	styles: [],
 	changeDetection: ChangeDetectionStrategy.OnPush
