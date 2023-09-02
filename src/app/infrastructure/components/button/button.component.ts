db5a8fcedea76ea97d79f08acfbeba4f01944afe
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-02 18:57:53 +0200

    remove unnecessary import

--- src/app/infrastructure/components/button/button.component.ts
+++ src/app/infrastructure/components/button/button.component.ts
@@ -1,11 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'challenger-button',
 	standalone: true,
-	imports: [CommonModule, MatButtonModule],
+	imports: [MatButtonModule],
 	templateUrl: './button.component.html',
 	styles: []
 })
