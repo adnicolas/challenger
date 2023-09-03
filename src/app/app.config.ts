e0c5df4a9e959f399e1d5c9979d24c1259166cb6
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-03 11:29:51 +0200

    remove bug comment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 export const appConfig: ApplicationConfig = {
 	providers: [
 		// eslint-disable-next-line max-len
-		// see: https://stackoverflow.com/questions/72504142/how-to-add-browseranimationsmodule-or-noopanimationsmodule-to-standalone-compone/72504735#72504735
 		importProvidersFrom([BrowserAnimationsModule])
 	]
 };
