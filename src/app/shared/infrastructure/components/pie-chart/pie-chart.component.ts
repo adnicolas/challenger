58f12732d88ea4847b0fef2ac9900d9fc9d91446
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-03 08:27:01 +0200

    add pie chart component

---
+++ src/app/shared/infrastructure/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-magic-numbers */
+import {
+	ChangeDetectionStrategy,
+	Component,
+	Input,
+	OnInit
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+	select as d3select,
+	scaleOrdinal as d3scaleOrdinal,
+	pie as d3pie,
+	arc as d3arc
+} from 'd3';
+@Component({
+	selector: 'challenger-pie-chart',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './pie-chart.component.html',
+	styles: [],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PieChartComponent implements OnInit {
+	@Input() data: any[] = [
+		{ Framework: 'Vue', Stars: '166443', Released: '2014' },
+		{ Framework: 'React', Stars: '150793', Released: '2013' },
+		{ Framework: 'Angular', Stars: '62342', Released: '2016' },
+		{ Framework: 'Backbone', Stars: '27647', Released: '2010' },
+		{ Framework: 'Ember', Stars: '21471', Released: '2011' }
+	];
+	@Input() margin = 50;
+	@Input() width = 750;
+	@Input() height = 600;
+	@Input() scaleProperty!: string;
+
+	private radius = Math.min(this.width, this.height) / 2 - this.margin;
+	private colors: any;
+	private svg: any;
+
+	ngOnInit(): void {
+		this.createSvg();
+		this.createColors();
+		this.drawChart();
+	}
+	private createSvg(): void {
+		this.svg = d3select('figure#pie')
+			.append('svg')
+			.attr('width', this.width)
+			.attr('height', this.height)
+			.append('g')
+			.attr('transform', `translate(${this.width / 2},${this.height / 2})`);
+	}
+
+	private createColors(): void {
+		this.colors = d3scaleOrdinal()
+			.domain(this.data.map((d: any) => d[this.scaleProperty].toString()))
+			.range([
+				'#c7d3ec',
+				'#a5b8db',
+				'#879cc4',
+				'#677795',
+				'#5a6782'
+			]);
+	}
+
+	private drawChart(): void {
+		const pie = d3pie<any>().value((d: any) => Number(d.Stars));
+
+		this.svg
+			.selectAll('pieces')
+			.data(pie(this.data))
+			.enter()
+			.append('path')
+			.attr('d', d3arc().innerRadius(0)
+				.outerRadius(this.radius))
+			.attr('fill', (d: any, i: any) => this.colors(i))
+			.attr('stroke', '#121926')
+			.style('stroke-width', '1px');
+
+		const labelLocation = d3arc().innerRadius(100)
+			.outerRadius(this.radius);
+
+		this.svg
+			.selectAll('pieces')
+			.data(pie(this.data))
+			.enter()
+			.append('text')
+			.text((d: any) => d.data.Framework)
+			.attr('transform', (d: any) => `translate(${labelLocation.centroid(d)})`)
+			.style('text-anchor', 'middle')
+			.style('font-size', 15);
+	}
+}
