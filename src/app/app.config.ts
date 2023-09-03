0d0f7db4ef37237d53a07a8304ddbef62c90aa03
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-03 11:30:29 +0200

    remove eslint rule disable comment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,5 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		// eslint-disable-next-line max-len
-		importProvidersFrom([BrowserAnimationsModule])
-	]
+	providers: [importProvidersFrom([BrowserAnimationsModule])]
 };
