186552cd0949e63aeee34363c41b90e18fc91eca
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-03 19:21:08 +0200

    remove dead code

--- src/app/heroes/application/SetTableColumnsNames.ts
+++ src/app/heroes/application/SetTableColumnsNames.ts
@@ -10,33 +10,5 @@ export class SetTableColumnsNames {
 	}
 	private getTableColumnsNamesFromHeroes(heroes: MarvelHero[]): string[] {
 		return Object.keys(heroes[0]).map((key) => key);
-		/*const tableColumns: TableColumn[] = Object.keys(heroes[0]).map(
-			(name: string) => {
-				const groupedData = this.groupBy(heroes, name);
-				const chartData: ChartData[] = [];
-				for (const [key, value] of Object.entries(groupedData)) {
-					chartData.push({
-						name: key,
-						// eslint-disable-next-line @typescript-eslint/no-explicit-any
-						value: (value as any[]).length
-					});
-				}
-				return {
-					name,
-					chartData
-				};
-			},
-		);
-		return tableColumns;*/
-	}
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	private groupBy(data: MarvelHero[], key: string): any {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return data.reduce((storage: any, item: MarvelHero) => {
-			const group = item[key as keyof MarvelHero];
-			storage[group] = storage[group] || [];
-			storage[group].push(item);
-			return storage;
-		}, {});
 	}
 }
