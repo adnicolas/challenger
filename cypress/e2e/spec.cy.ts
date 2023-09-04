4e65b9a3a20d4c05c1de1ccced6f09e2415f990b
Adrián Nicolás <adrian.nicolas@geograma.com>
2023-09-04 16:20:50 +0200

    add cypress and some tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,27 @@
+/* eslint-disable max-lines-per-function */
+describe('Challenger tests', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+	it('Contains the Create hero button', () => {
+		cy.contains('button', 'Create hero');
+	});
+	it('Opens HeroCreationDialog when click on the button', () => {
+		cy.contains('button', 'Create hero').click(); // Click on first el containing 'Welcome'
+		cy.get('div').should('have.class', 'hero-creation-dialog-main-container');
+		cy.contains('button', 'Save');
+		cy.contains('button', 'Close');
+	});
+	it('Contains the Ahab row', () => {
+		cy.contains('td', 'Ahab');
+	});
+	it('Opens HeroDetailDialog when click on the row', () => {
+		cy.contains('td', 'Ahab').click();
+		cy.get('div').should('have.class', 'hero-detail-dialog-main-container');
+		cy.contains('button', 'Close');
+		cy.contains('div', 'Ahab');
+	});
+	it('Shows the charts', () => {
+		cy.get('svg').should('have.class', 'ngx-charts');
+	});
+});
